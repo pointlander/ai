43ca5f462f62dfb37ce6be07dcb9d9e8e3a5080b
Andrew Snodgrass <git@pointlander.info>
2022-06-12 19:53:06 -0500

    Simple transformer

--- go.mod
+++ go.mod
@@ -2,10 +2,20 @@ module github.com/pointlander/ai
 
 go 1.18
 
+
 require (
+	github.com/pointlander/datum/iris v0.0.0-20200802052503-0ee610caba95
+	github.com/pointlander/gradient v0.0.0-20220613004418-0e1851389f05
+	gonum.org/v1/plot v0.11.0
+	git.sr.ht/~sbinet/gg v0.3.1 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/go-fonts/liberation v0.2.0 // indirect
+	github.com/go-latex/latex v0.0.0-20210823091927-c0d11ff05a81 // indirect
+	github.com/go-pdf/fpdf v0.6.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/pointlander/datum/iris v0.0.0-20200802052503-0ee610caba95 // indirect
-	github.com/pointlander/gradient v0.0.0-20220607014908-46340a667d10 // indirect
 	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb // indirect
+	golang.org/x/image v0.0.0-20220302094943-723b81ca9867 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.24.0 // indirect
 )
--- main.go
+++ main.go
@@ -11,6 +11,10 @@ import (
 
 	"github.com/pointlander/datum/iris"
 	"github.com/pointlander/gradient/tf32"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 )
 
 // Statistics captures statistics
@@ -64,5 +68,104 @@ func main() {
 				w.X = append(w.X, float32(measure))
 			}
 		}
+	}
+
+	set := tf32.NewSet()
+	set.Add("query", 4, 4)
+	set.Add("key", 4, 4)
+	set.Add("value", 4, 4)
+	set.Add("project", 4, 4)
+
+	for _, w := range set.Weights {
+		factor := math.Sqrt(2.0 / float64(w.S[0]))
+		for i := 0; i < cap(w.X); i++ {
+			w.X = append(w.X, float32(rnd.NormFloat64()*factor))
+		}
+	}
+
+	deltas := make([][]float32, 0, 8)
+	for _, p := range set.Weights {
+		deltas = append(deltas, make([]float32, len(p.X)))
+	}
+
+	query := tf32.Mul(set.Get("query"), others.Get("input"))
+	key := tf32.Mul(set.Get("key"), others.Get("input"))
+	value := tf32.Mul(set.Get("value"), others.Get("input"))
+	transformer := tf32.Mul(set.Get("project"),
+		tf32.Hadamard(tf32.Sigmoid(query),
+			tf32.SumRows(tf32.Hadamard(tf32.Softmax(key), value))))
+	cost := tf32.Avg(tf32.Quadratic(transformer, others.Get("output")))
+
+	alpha, eta, iterations := float32(.1), float32(.1), 300
+	points := make(plotter.XYs, 0, iterations)
+	i := 0
+	for i < iterations {
+		total := float32(0.0)
+		set.Zero()
+		others.Zero()
+
+		/*if i == 128 || i == 2*128 || i == 3*128 || i == 4*128 {
+			for j := range d {
+				d[j] /= 10
+			}
+		}
+
+		index := 0
+		for _, data := range iris {
+			for i, measure := range data.Measures {
+				if d[i] == 0 {
+					inputs.X[index] = float32(measure)
+				} else {
+					inputs.X[index] = float32(measure + rnd.NormFloat64()*d[i])
+				}
+				index++
+			}
+		}*/
+
+		total += tf32.Gradient(cost).X[0]
+		sum := float32(0.0)
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				sum += d * d
+			}
+		}
+		norm := float32(math.Sqrt(float64(sum)))
+		scaling := float32(1.0)
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+
+		for j, w := range set.Weights {
+			for k, d := range w.D {
+				deltas[j][k] = alpha*deltas[j][k] - eta*d*scaling
+				set.Weights[j].X[k] += deltas[j][k]
+			}
+		}
+
+		points = append(points, plotter.XY{X: float64(i), Y: float64(total)})
+		fmt.Println(i, total)
+		/*if total < .1 {
+			break
+		}*/
+		i++
+	}
+
+	p := plot.New()
+
+	p.Title.Text = "epochs vs cost"
+	p.X.Label.Text = "epochs"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "cost.png")
+	if err != nil {
+		panic(err)
 	}
 }
